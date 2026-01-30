e458224958025cdd9549b437ac04cbb414e73dd9
pareraamas <pareraamas@gmail.com>
2026-01-31 04:20:41 +0700

    update depedency

--- lib/src/bindings/common_binding.dart
+++ lib/src/bindings/common_binding.dart
@@ -1,8 +1,7 @@
+import 'package:app_common/app_common.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:get/get.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import '../infrastructure/network/api_client.dart';
-import '../infrastructure/storage/auth_session.dart';
-import '../config/config_core.dart';
 
 /// Global binding for core infrastructure services.
 /// This should be used in the main app's initialBinding or GetMaterialApp(initialBinding: CommonBinding()).
@@ -11,28 +10,20 @@ class CommonBinding extends Bindings {
   void dependencies() {
     // 1. Secure Storage
     if (!Get.isRegistered<FlutterSecureStorage>()) {
-      Get.put(
-        const FlutterSecureStorage(
-          aOptions: AndroidOptions(encryptedSharedPreferences: true),
-        ),
-        permanent: true,
-      );
+      Get.put(const FlutterSecureStorage(aOptions: AndroidOptions(encryptedSharedPreferences: true)), permanent: true);
     }
 
     // 2. Auth Session (Plain class, manually managed or via Get.put)
     final storage = Get.find<FlutterSecureStorage>();
-    final authSession = Get.put<IAuthSession>(
-      AuthSession(storage: storage),
-      permanent: true,
-    );
+    final authSession = Get.put<IAuthSession>(AuthSession(storage: storage), permanent: true);
 
     // 3. API Client
-    Get.put(
-      ApiClient(
-        baseUrl: ConfigCore.apiBaseUrl,
-        authSession: authSession,
-      ),
-      permanent: true,
-    );
+    Get.put(ApiClient(baseUrl: ConfigCore.apiBaseUrl, authSession: authSession), permanent: true);
+
+    // 4. repositories and other services can be added here similarly
+    Get.putAsync<AuthRemoteDataSource>(() async {
+      // Example: return SomeRepository(apiClient: Get.find<ApiClient>());
+      return AuthRemoteDataSourceImpl(Get.find<ApiClient>(), authSession, Get.put<DeviceInfoPlugin>(DeviceInfoPlugin()));
+    }, permanent: true);
   }
 }
